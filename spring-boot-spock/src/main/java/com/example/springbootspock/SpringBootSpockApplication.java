687136f964279bfdc28a7a5528395c5121d6abaa
haeny-dev <wjdeoaks92@gmail.com>
2022-07-07 15:39:22 +0900

    springboot-spock: junit 버전 계산기 테스트코드 작성

---
+++ spring-boot-spock/src/main/java/com/example/springbootspock/SpringBootSpockApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootspock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSpockApplication.class, args);
+    }
+
+}
---
+++ spring-boot-spock/src/test/java/com/example/springbootspock/SpringBootSpockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootspock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSpockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
